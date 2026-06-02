c9795bfd23f201c8206a467f165c8946bdb9e584
skdeveloper1415 <khan446688@gmail.com>
2026-06-02 11:45:25 +0530

    Swiper

--- app/components/BlogSection.tsx
+++ app/components/BlogSection.tsx
@@ -1,129 +1,116 @@
 "use client";
-import Slider from "react-slick";
-import Image from "next/image";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 import { useRef } from "react";
+import Image from "next/image";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
 import IndustryCard from "./ui/BlogCard";
 import { blogs } from "@/app/data/blogs";
 
 export default function BlogSection() {
-  const sliderRef = useRef<Slider | null>(null);
-  
-  const settings = {
-    className: "",
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    adaptiveHeight: true,
-    autoplay: false,
-    dots: false,
-    arrows: false, // Important
-    mobileFirst: true,
-    responsive: [      
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 1280,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 1536,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-    ],
-  };
+  const swiperRef = useRef<any>(null);
+
   return (
-    <>
     <section className="py-16 blog_bg max-lg:px-5">
       <div className="max-w-7xl mx-auto">
-      <div className="text-black font-semibold text-3xl flex items-center justify-between">
-        <h5>More on Pyramid Talent</h5>
-        {/* Custom Controls */}
+
+        {/* Header */}
+        <div className="text-black font-semibold text-3xl flex items-center justify-between">
+          <h5>More on Pyramid Talent</h5>
+
+          {/* Custom Controls */}
           <div className="flex gap-4">
             <button
-              onClick={() => sliderRef.current?.slickPrev()}
+              onClick={() => swiperRef.current?.slidePrev()}
               className="
-              cursor-pointer
-              w-14
-              h-14
-              rounded-full
-              border
-              border-gray-300
-              bg-lime-400
-              flex
-              items-center
-              justify-center
-              transition
+                cursor-pointer
+                w-14
+                h-14
+                rounded-full
+                border
+                border-gray-300
+                bg-lime-400
+                flex
+                items-center
+                justify-center
               "
             >
               <Image
-                src={"/assets/arrow-narrow-left.svg"}
-                alt="Next"
+                src="/assets/arrow-narrow-left.svg"
+                alt="Previous"
                 width={16}
                 height={16}
               />
             </button>
 
             <button
-              onClick={() => sliderRef.current?.slickNext()}
+              onClick={() => swiperRef.current?.slideNext()}
               className="
-              cursor-pointer
-              w-14
-              h-14
-              rounded-full
-              border
-              border-gray-300
-              bg-lime-400
-              flex
-              items-center
-              justify-center
-              transition
+                cursor-pointer
+                w-14
+                h-14
+                rounded-full
+                border
+                border-gray-300
+                bg-lime-400
+                flex
+                items-center
+                justify-center
               "
             >
               <Image
-                src={"/assets/arrow-narrow-right.svg"}
+                src="/assets/arrow-narrow-right.svg"
                 alt="Next"
                 width={16}
                 height={16}
               />
             </button>
           </div>
-      </div>
-        <div className="my-10">
-    <Slider ref={sliderRef} {...settings}>
-       {blogs.map((item) => (
-        <div
-          key={item.id}
-          className="px-4"
-        >
-          <IndustryCard
-            category={item.category}
-            image={item.image}
-            date={item.date}
-            comments={item.comments}
-            title={item.title}
-          />
         </div>
-      ))}
-    </Slider>
-    </div>
-    </div>
+
+        {/* Slider */}
+        <div className="my-10">
+          <Swiper
+            modules={[Navigation]}
+            spaceBetween={24}
+            loop={true}
+            onSwiper={(swiper) => {
+              swiperRef.current = swiper;
+            }}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+              },
+
+              768: {
+                slidesPerView: 2,
+              },
+
+              1024: {
+                slidesPerView: 3,
+              },
+            }}
+          >
+            {blogs.map((item) => (
+              <SwiperSlide key={item.id}>
+                <IndustryCard
+                  category={item.category}
+                  image={item.image}
+                  date={item.date}
+                  comments={item.comments}
+                  title={item.title}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+
+      </div>
     </section>
-    </>
-    )
+  );
 }
